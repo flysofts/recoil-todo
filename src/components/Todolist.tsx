41b339b6eeec2c9e3ee8695470919c60024fe085
flysofts <ds4950@naver.com>
2024-06-20 18:25:10 +0900

    수정

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,127 +1,146 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
-// function Todolist() {
-//   const [todo, setTodo] = useState("");
-//   const [toDoError, setToDoError] = useState("");
-//   const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-//     const {
-//         currentTarget: {value},
-//     } = event;
-//     setToDoError("");
-//     setTodo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
-//     event.preventDefault();
-//     if(todo.length < 10) {
-//       return setToDoError("To do should be longer");
-//     }
-//     console.log("submit");
-//   }
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={todo} type="text" placeholder="Write a to do" />
-//         <button>Add</button>
-//         {toDoError !== "" ? toDoError :null}
-//       </form>
-//     </div>
-//   );
-// }
+import { atom, useRecoilState } from "recoil";
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
-function Todolist() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "Server offline." });
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
   };
-  console.log(errors);
   return (
     <div>
-    <form
-      style={{ display: "flex", flexDirection: "column" }}
-      onSubmit={handleSubmit(onValid)}
-    >
-      <input
-        {...register("email", {
-          required: "Email is required",
-          pattern: {
-            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-            message: "Only naver.com emails allowed",
-          },
-        })}
-        placeholder="Email"
-      />
-      <span>{errors?.email?.message}</span>
-      <input
-        {...register("firstName", {
-          required: "write here",
-          validate: {
-            noNico: (value) =>
-              value.includes("my") ? "no nicos allowed" : true,
-            noNick: (value) =>
-              value.includes("name") ? "no nick allowed" : true,
-          },
-        })}
-        placeholder="First Name"
-      />
-      <span>{errors?.firstName?.message}</span>
-      <input
-        {...register("lastName", { required: "write here" })}
-        placeholder="Last Name"
-      />
-      <span>{errors?.lastName?.message}</span>
-      <input
-        {...register("username", { required: "write here", minLength: 10 })}
-        placeholder="Username"
-      />
-      <span>{errors?.username?.message}</span>
-      <input
-        {...register("password", { required: "write here", minLength: 5 })}
-        placeholder="Password"
-      />
-      <span>{errors?.password?.message}</span>
-      <input
-        {...register("password1", {
-          required: "Password is required",
-          minLength: {
-            value: 5,
-            message: "Your password is too short.",
-          },
-        })}
-        placeholder="Password1"
-      />
-      <span>{errors?.password1?.message}</span>
-      <button>Add</button>
-      <span>{errors?.extraError?.message}</span>
-    </form>
-  </div>
-);
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Todolist;
+// interface IForm {
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+//   username: string;
+//   password: string;
+//   password1: string;
+//   extraError?: string;
+// }
+
+// function Todolist() {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//     setError,
+//   } = useForm<IForm>({
+//     defaultValues: {
+//       email: "@naver.com",
+//     },
+//   });
+//   const onValid = (data: IForm) => {
+//     if (data.password !== data.password1) {
+//       setError(
+//         "password1",
+//         { message: "Password are not the same" },
+//         { shouldFocus: true }
+//       );
+//     }
+//     // setError("extraError", { message: "Server offline." });
+//   };
+//   console.log(errors);
+//   return (
+//     <div>
+//     <form
+//       style={{ display: "flex", flexDirection: "column" }}
+//       onSubmit={handleSubmit(onValid)}
+//     >
+//       <input
+//         {...register("email", {
+//           required: "Email is required",
+//           pattern: {
+//             value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+//             message: "Only naver.com emails allowed",
+//           },
+//         })}
+//         placeholder="Email"
+//       />
+//       <span>{errors?.email?.message}</span>
+//       <input
+//         {...register("firstName", {
+//           required: "write here",
+//           validate: {
+//             noNico: (value) =>
+//               value.includes("my") ? "no nicos allowed" : true,
+//             noNick: (value) =>
+//               value.includes("name") ? "no nick allowed" : true,
+//           },
+//         })}
+//         placeholder="First Name"
+//       />
+//       <span>{errors?.firstName?.message}</span>
+//       <input
+//         {...register("lastName", { required: "write here" })}
+//         placeholder="Last Name"
+//       />
+//       <span>{errors?.lastName?.message}</span>
+//       <input
+//         {...register("username", { required: "write here", minLength: 10 })}
+//         placeholder="Username"
+//       />
+//       <span>{errors?.username?.message}</span>
+//       <input
+//         {...register("password", { required: "write here", minLength: 5 })}
+//         placeholder="Password"
+//       />
+//       <span>{errors?.password?.message}</span>
+//       <input
+//         {...register("password1", {
+//           required: "Password is required",
+//           minLength: {
+//             value: 5,
+//             message: "Your password is too short.",
+//           },
+//         })}
+//         placeholder="Password1"
+//       />
+//       <span>{errors?.password1?.message}</span>
+//       <button>Add</button>
+//       <span>{errors?.extraError?.message}</span>
+//     </form>
+//   </div>
+// );
+// }
+
+export default ToDoList;
