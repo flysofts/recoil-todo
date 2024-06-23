eb81941ad7df69e95c0da05d7bd7e48730b7ac45
flysofts <ds4950@naver.com>
2024-06-23 21:07:12 +0900

    category

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function Todo({ text }: IToDo) {
+
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
-    <div>
-      <li>
-        <span>{text}</span>
-        <button>Doing</button>
-        <button>To Do</button>
-        <button>Done</button>
-      </li>
-    </div>
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
   );
 }
 
-export default Todo;
+export default ToDo;
